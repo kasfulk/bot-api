3f56042f3a438e0cd55becf628dbc390624adff7
Kasjful Kurniawan <kasful.bpp@gmail.com>
2021-12-23 09:24:14 +0800

    feat: notif naru

--- src/controllers/controller-naru.js
+++ src/controllers/controller-naru.js
@@ -33,7 +33,7 @@ const sendPiket = async (req, res) => {
         const dataShow = rows.map(row => {
             return `*${row.divisi}:*\n_${row.nama}_ (+62${row.nomor})\n`;
         });
-        const message = `*Piket Naru 2021*\n\n${tanggalShow}\n${shiftFunction(shift)} \n\n${dataShow.join("\n")}`;
+        const message = `*Piket Naru 2021*\n\n${tanggalShow}\n${shiftFunction(shift)} \n\n${dataShow.join("\n")}\n\n*Dashboard HQ:*\nhttp://10.54.36.55:50000/dessy\n- user: admin\n- pass: ADMportal#2021\nLink per fitur:\n1. Subscribers Movement: \nhttp://10.54.36.55/dessy-movement/public/\n2. POI Monitoring (dilakukan share screen oleh Co-leader Posko): \nhttp://10.54.36.55:50000/dessy-naru\n3. Network Performance: \nhttp://10.54.36.55:3006/naru/home\n\n*Dashboard Regional:*\nhttps://kalimantan.telkomsel.co.id/grafana/d/nobYOFOnk/dashboard-naru-kalimantan-2021?orgId=1\n\n*Absen logbook NARU HQ*\nhttp://10.54.36.55:9004/dessy-tracking/login\n- user: all_activity\n- pass: all_activity#2021\n\n*Link Vicon HQ*\nhttp://tsel.me/PoskoNARU2021\n*Link Vicon HQ Backup*\nhttp://bit.ly/BackupRoomNaru2021\n*Link Vicon Telkom Group*\nhttps://bit.ly/PoskoNARU2021Telkom\n- passcode naru21222\n*Link Vicon Regional-NS*\nhttps://us02web.zoom.us/j/5673105992?pwd=S1lFYUpHUldvU2UxTFV2a2RqbmdnUT09\nMeeting ID: 567 310 5992 \nPasscode: SbN24V\n*Link gdocs Posko Regional NS untuk update MSI/Ticket/POI down*\nhttps://docs.google.com/spreadsheets/d/1_IEZWUCArXQH2vcYzH7dHY1_azFgUlnevoRcoBKsw88/edit#gid=0`;
         
         if (rows.length > 0) {
             const listGroup = [
